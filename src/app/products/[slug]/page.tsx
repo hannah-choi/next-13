7a9f8e8dab45508beed202fa09ab50d03194c798
hannah-developer <hannahchoi.developer@gmail.com>
2023-04-10 17:10:23 +0100

    linting

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 type Props = {
-  params: {
-    slug: string;
-  }
+    params: {
+        slug: string;
+    };
+};
+
+export default function ProductItem({ params }: Props) {
+    return <h1>{params.slug} details</h1>;
 }
 
-
-export default function Trousers({params}: Props) {
-  return (
-    <h1>{params.slug}</h1>
-  )
+export function generateStaticParams() {
+    //다이나믹 라우팅을 하면서 특정 페이지만 정적 라우팅을 하고싶을때
+    const products = ['Shirt', 'Skirt'];
+    return products.map((product) => ({
+        slug: product
+    }));
 }
-
